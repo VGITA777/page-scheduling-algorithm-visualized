144892036fe765d9e0ab5c373c4900c843689a48
Horizon <113239025+VGITA777@users.noreply.github.com>
2024-11-30 18:32:54 +0800

    Models Changes

--- src/main/java/com/prince/schedulingalgogui/models/HomePageTableData.java
+++ src/main/java/com/prince/schedulingalgogui/models/HomePageTableData.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 @Getter
 public class HomePageTableData {
     private final int frameCount;
+    private final boolean isCurrentColumnFrameCount;
     private final SchedulerResult schedulerResult;
 
-    public HomePageTableData(int frameCount, SchedulerResult schedulerResult) {
+    public HomePageTableData(int frameCount, SchedulerResult schedulerResult , boolean isCurrentColumnFrameCount) {
         this.frameCount = frameCount;
         this.schedulerResult = schedulerResult;
+        this.isCurrentColumnFrameCount = isCurrentColumnFrameCount;
     }
 }
