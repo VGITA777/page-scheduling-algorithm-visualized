7991bba0aa5f0201f71d5b755fdd114ce6c575b5
VGITA777 <113239025+VGITA777@users.noreply.github.com>
2024-12-01 22:41:36 +0800

    Import optimizations

--- src/main/java/com/prince/schedulingalgogui/App.java
+++ src/main/java/com/prince/schedulingalgogui/App.java
@@ -16,8 +16,6 @@ import javax.swing.*;
 public class App extends Application {
     private static void setupStage(@NonNull Stage stage) {
         stage.setTitle("Scheduling Algorithm Visualizer");
-        stage.setMinWidth(1100);
-        stage.setMaxWidth(1500);
         stage.setMinHeight(600);
         stage.setMaxHeight(800);
         stage.setOnCloseRequest(_ -> JOptionPane.showMessageDialog(null, "Goodbye, user!"));
